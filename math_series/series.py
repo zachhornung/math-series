458c80e4d62de2feae3af215335e20567956b1e2
Zach Hornung <Zachhornung@comcast.net>
2021-05-19 00:13:13 -0700

    code passes tests

--- math_series/series.py
+++ math_series/series.py
@@ -1,2 +1,23 @@
 def hello():
-    print('hello world')
+    return 'hello world'
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
+
+def lucas(n):
+    if n == 0:
+        return 2
+    if n == 1:
+        return 1
+    return lucas(n-1) + lucas(n-2)
+
+def sum_series(n, a=0, b=1):
+    if n == 0:
+        return a
+    if n == 1:
+        return b
+    return sum_series(n-1, a, b) + sum_series(n-2, a, b)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,10 +1,31 @@
-
-print('hello world')
 import pytest
-from math_series.series import hello
+from math_series.series import hello, fibonacci, lucas, sum_series
 
-def test_one():
+def test_proof_life():
     actual = hello()
     expected = 'hello world'
     assert actual == expected
-test_one()
+
+def test_fib_1():
+    actual = fibonacci(5)
+    expected = 5
+    assert actual == expected
+
+def test_fib_2():
+    actual = fibonacci(6)
+    expected = 8
+    assert actual == expected
+
+def test_lucas_1():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
+def test_sum_series_1():
+    actual = sum_series(5)
+    expected = 5
+    assert actual == expected
+
+def test_sum_series_2():
+    actual = sum_series(5, 2, 1)
+    expected = 11
+    assert actual == expected
